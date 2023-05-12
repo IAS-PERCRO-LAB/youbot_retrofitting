4593b74e4ff7873933b4f357655a383b46648bf8
AlbertoRemus <alberto.remus@santannapisa.it>
2023-05-12 18:32:19 +0200

    Removing CMake compiled files not needed in the project

--- youbot_ws/src/youbot_driver/youbot/CMakeFiles/YouBotDriverShared.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../lib/libYouBotDriver.pdb"
-  "../lib/libYouBotDriver.so"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/YouBotDriverShared.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- youbot_ws/src/youbot_driver/youbot/testing/CMakeFiles/base_arm_gripper_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for base_arm_gripper_test.
-# This may be replaced when dependencies are built.
